432b803d44a396e371d2c31ba9383731aeaec64c
lichenhaunt <lichenhaunt@yahoo.com>
2024-08-10 21:27:54 -0400

    initial commit

---
+++ themes/v1/theme.js
@@ -0,0 +1,18 @@
+// this code was copied from dogmeat.com, shoutout Des for this one fr
+
+window.onload = checkTheme();
+
+function pickTheme(value) {
+  var sheet = document.getElementsByClassName("theme")[0];
+  sheet.href = value;
+  localStorage.setItem("theme", value);
+}
+
+function checkTheme() {
+  var sheet = document.getElementsByClassName("theme")[0];
+  var localTheme = localStorage.getItem("theme");
+  sheet.href = localTheme;
+  if (localTheme == null) {
+    sheet.href = "!f/v4/sunset.css";
+  }
+}
