74d60185011024dd5f65a3199554f3efb7c84c3d
lichenhaunt <lichenhaunt@gmail.com>
2024-09-02 16:33:48 -0400

    merged my trainer card html/css with dogmeat yukika template, card functionality yay

--- about/script.js
+++ about/script.js
@@ -1,19 +1,64 @@
-let card = document.getElementsByClassName("card");
-let front = document.getElementById("front");
-let back = document.getElementById("back");
-let flipFront = document.getElementById("front-btn");
-let flipBack = document.getElementById("back-btn");
+// cloned from Dogmeat
+//	the important thing here is that it works tbh
 
-function frontFlip() {
-  front.style.display = "inline-block";
-  back.style.display = "none";
-  flipFront.onclick = front;
-  flipBack.onclick = backFlip();
+let passport = document.getElementById("cover");
+let passportOpened = document.getElementById("passport-opened");
+let page1 = document.getElementById("page-1");
+let page2 = document.getElementById("page-2");
+let btn1 = document.getElementById("beforeBtn");
+let btn2 = document.getElementById("afterBtn");
+let otherPages = document.getElementsByClassName("page");
+
+// added this bit because my css made everything visible on first load, this hides everything other than passport cover on page load
+window.onload = () => {
+  passport.style.display = "block";
+  passportOpened.style.display = "none";
+  btn1.style.display = "none";
+  btn2.style.display = "none";
+};
+
+function passportOpen() {
+  btn1.style.display = "inline-block";
+  btn2.style.display = "inline-block";
+  passportOpened.style.display = "block";
+  page1.style.display = "block";
+  page2.style.display = "none";
+  passport.className = "passportANI";
+  passportOpened.className = "pageANI";
+  btn1.onclick = passportClose;
+  btn1.innerText = "close passport";
+  btn2.onclick = passportPage2;
+  btn2.innerText = "next page";
 }
 
-function backFlip() {
-  back.style.display = "inline-block";
-  front.style.display = "none";
-  flipFront.onclick = frontFlip();
-  flipBack = back;
+function passportPage1() {
+  page1.className = "pageTurnANI1";
+  passportOpened.style.display = "block";
+  page1.style.display = "block";
+  page2.style.display = "none";
+  passportOpened.classList.remove("pageANI");
+  btn1.onclick = passportClose;
+  btn1.innerText = "close passport";
+  btn2.onclick = passportPage2;
+  btn2.innerText = "next page";
+  passport.style.display = "none";
+}
+
+function passportPage2() {
+  page2.className = "pageTurnANI2";
+  page1.style.display = "none";
+  page2.style.display = "block";
+  btn1.onclick = passportPage1;
+  btn1.innerText = "previous page";
+  btn2.onclick = passportClose;
+  btn2.innerText = "close passport";
+  passport.style.display = "none";
+}
+
+function passportClose() {
+  passport.style.display = "block";
+  passport.className = "passportCloseANI";
+  passportOpened.style.display = "none";
+  btn1.style.display = "none";
+  btn2.style.display = "none";
 }
